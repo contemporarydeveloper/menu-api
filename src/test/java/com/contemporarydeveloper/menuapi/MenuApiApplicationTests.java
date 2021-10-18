09ab809350451901bd82b694d8053a4472725735
Contemporary Developer <contemporarydeveloper@gmail.com>
2021-10-18 10:12:34 +0100

    Add test directory

---
+++ src/test/java/com/contemporarydeveloper/menuapi/MenuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contemporarydeveloper.menuapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
