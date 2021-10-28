20fcd01a1db9dfdc584394680fb36cc8c4419947
Contemporary Developer <contemporarydeveloper@gmail.com>
2021-10-28 15:03:21 +0100

    Add successful get menu by id test and generateMenu helper method

--- src/test/java/com/contemporarydeveloper/menuapi/controllers/MenuControllerTest.java
+++ src/test/java/com/contemporarydeveloper/menuapi/controllers/MenuControllerTest.java
@@ -1,6 +1,8 @@
 package com.contemporarydeveloper.menuapi.controllers;
 
+import com.contemporarydeveloper.menuapi.models.Menu;
 import com.contemporarydeveloper.menuapi.services.MenuService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,11 +21,18 @@ public class MenuControllerTest {
     private static final String MENU_ID = "1234";
     private static final String MENU_NAME = "dinner";
 
+    private Menu menu;
+
     @Mock
     private MenuService menuService;
 
     @InjectMocks
     private MenuController menuController;
+
+    @BeforeEach
+    void setUp() {
+        menu = generateMenu();
+    }
 
     @Test
     @DisplayName("Unsuccessful get menu by id - not found")
@@ -34,5 +43,23 @@ public class MenuControllerTest {
 
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
         assertNull(response.getBody());
+    }
+
+    @Test
+    @DisplayName("Successful get menu by id")
+    void getMenuByIdSuccessful() {
+        when(menuService.findMenuById(MENU_ID)).thenReturn(menu);
+
+        ResponseEntity response = menuController.getMenuById(MENU_ID);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(menu, response.getBody());
+    }
+
+    private Menu generateMenu() {
+        Menu menu = new Menu();
+        menu.setId(MENU_ID);
+        menu.setName(MENU_NAME);
+        return menu;
     }
 }
